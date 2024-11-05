2bf145cb14b3d4f44c5ba028cdd04bfbb6bd5ec3
Admin <Admin@ADMIN-PC>
2024-11-05 20:18:11 +0700

    Init code 2

---
+++ src/Components/Users/Login.jsx
@@ -0,0 +1,42 @@
+import { React } from "react";
+import  "./Login";
+
+var MyClass = React.createClass({
+    render: function() {
+      return (
+        <div>
+          <meta charSet="UTF-8" />
+          <link rel="icon" type="image/svg+xml" href="/vite.svg" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <title> Login </title>
+          <link rel="Loginsheet" href="Login.css" />
+          <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="Loginsheet" />
+          <div id="root" />
+          <div className="wrapper">
+            <form action> 
+              <h1>Login</h1>
+              <style dangerouslySetInnerHTML={{__html: " h1 {font-size: 1000px; } " }} />
+              <div className="input-box">
+                <input type="text" placeholder="Username" required />
+                <i className="bx bxs-user" />
+              </div>
+              <div className="input-box">
+                <input type="password" placeholder="Password" required />
+                <i className="bx bxs-lock-alt" />
+              </div>
+              <div className="remember-forgot">
+                <label> <input type="checkbox" /> Remeber me </label>
+                <a href="#"> Forgot password? </a>
+              </div>
+              <button type="submit" className="btn"> Login </button>
+              <div className="register-link">
+                <p> Don't have an account? <a href="#"> Register </a></p>
+              </div>
+            </form>
+          </div>
+        </div>
+      );
+    }
+  });
+
+  export default Login;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Login from './Components/Users/Login.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
+
+createLogin(document.getElementById('login')).render(
+  <StrictMode>
+    <Login />
+  </StrictMode>, )
+  
 )
